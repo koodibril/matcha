764638d824eea16a971e2422dbf0285397abab10
AlphaBril <florian.marie.doucet@gmail.com>
2020-06-11 20:38:01 +0200

    tuto finis

---
+++ jernapp/controllers/userControllers.js
@@ -0,0 +1,8 @@
+module.exports = function (app) {
+    const account = require('./account/lib.js');
+
+    module.exports = function (app) {
+        app.post('/login',account.login);
+        app.post('/signup',account.signup);
+    }
+}
