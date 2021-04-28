95c3e4c0dd6606c5ccdc909bdf3b4ba5d955a5c1
AlphaBril <florian.marie.doucet@gmail.com>
2021-04-28 21:57:31 +0200

    added update success message

--- app/src/components/Home/Home.tsx
+++ app/src/components/Home/Home.tsx
@@ -21,45 +21,36 @@ const Home: React.FC = () => {
       getSearchResult(user);
   }, [user, getSearchResult]);
 
-  const handleSort = (tag: string, sense: boolean) => {
-    console.log('sort by');
-    console.log(tag);
-    if (sense === true)
-      console.log('ascending');
-    else
-      console.log('descending');
-  }
-
   const handleChange = (tag: any, checked: any) => {
     setSelectedTags([tag]);
+    if (tag !== selectedTags[0])
+      setDouble(false);
     if (checked === false && double === true) {
       setSelectedTags([]);
       setDouble(false);
     }
     if (checked === false && double === false) {
-      setDouble(true);
-      handleSort(tag, true);
+      setDouble(true); 
     }
-    handleSort(tag, false);
   };
 
   return (
     <>
-    <Row justify="center" align="middle">
-      <Divider plain>Sort</Divider>
-      {tagsData.map(tag => (
-        <Tag.CheckableTag
-          key={tag}
-          checked={selectedTags.indexOf(tag) > -1}
-          onChange={checked => handleChange(tag, checked)}>
-            {tag} {double && selectedTags.indexOf(tag) > -1 ? <UpOutlined></UpOutlined> : <DownOutlined></DownOutlined>}
-        </Tag.CheckableTag>
-      ))}
-    </Row>
-    <Divider plain></Divider>
-    <Row justify="center" align="middle">
-        <DisplayComponent userList={userList}></DisplayComponent>
-    </Row>
+      <Row justify="center" align="middle">
+        <Divider plain style={{width: "80%"}}>Sort</Divider>
+        {tagsData.map(tag => (
+          <Tag.CheckableTag
+            key={tag}
+            checked={selectedTags.indexOf(tag) > -1}
+            onChange={checked => handleChange(tag, checked)}>
+              {tag} {double && selectedTags.indexOf(tag) > -1 ? <UpOutlined></UpOutlined> : <DownOutlined></DownOutlined>}
+          </Tag.CheckableTag>
+        ))}
+      </Row>
+      <Divider style={{width: "80%"}}></Divider>
+      <Row justify="center" align="middle">
+          <DisplayComponent userList={userList}></DisplayComponent>
+      </Row>
     </>);
 };
 
