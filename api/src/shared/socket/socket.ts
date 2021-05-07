ad36c3f82839bde0d3f5d225c72795f1d1072207
AlphaBril <florian.marie.doucet@gmail.com>
2021-05-07 15:51:45 +0200

    removed testing ft from sockets

--- api/src/shared/socket/socket.ts
+++ api/src/shared/socket/socket.ts
@@ -1,12 +1,8 @@
 import { updateUserSocket } from "./registerSocket";
 
 module.exports = (io: any, socket: any) => {
-  const test = function (payload: any) {
-    console.log("hghghghghgh");
-  };
   const update = function (payload: any) {
     updateUserSocket(payload, socket.id);
   };
-  socket.on("order:test", test);
   socket.on("order:update", update);
 }
